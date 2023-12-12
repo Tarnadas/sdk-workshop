00f89a8275431269136c00ea971c487cdfbce019
Mario Reder <mario.reder@pm.me>
2023-12-12 08:58:47 +0100

    fix deps and refactor

--- src/Account.tsx
+++ src/Account.tsx
@@ -1,55 +1,26 @@
 import { useAccount } from '@orderly.network/hooks';
 import { AccountStatusEnum } from '@orderly.network/types';
 import { Button, Flex } from '@radix-ui/themes';
-import { BrowserProvider, JsonRpcSigner } from 'ethers';
-import { FC, useEffect, useState } from 'react';
+import { JsonRpcSigner } from 'ethers';
+import { FC, useEffect } from 'react';
 
-import { checkValidNetwork, testnetChainIdHex } from './network';
+import { testnetChainIdHex } from './network';
 
-export const Account: FC = () => {
-  const [provider, setProvider] = useState<BrowserProvider | undefined>();
-  const [signer, setSigner] = useState<JsonRpcSigner | undefined>();
-
+export const Account: FC<{ signer?: JsonRpcSigner }> = ({ signer }) => {
   const { account, state } = useAccount();
 
   useEffect(() => {
-    async function run() {
-      if (!window.ethereum) return;
-      const p = new BrowserProvider(window.ethereum);
-      setProvider(p);
-      const s = await p.getSigner();
-      setSigner(s);
-
-      await account.setAddress(s.address, {
-        provider: window.ethereum,
-        chain: {
-          id: testnetChainIdHex
-        }
-      });
-    }
-    run();
-  }, [account]);
-
-  useEffect(() => {
-    if (!provider) return;
-    checkValidNetwork(provider);
-  }, [provider]);
+    if (!signer) return;
+    account.setAddress(signer.address, {
+      provider: window.ethereum,
+      chain: {
+        id: testnetChainIdHex
+      }
+    });
+  }, [signer, account]);
 
   return (
-    <Flex gap="3" align="center" justify="center">
-      <Button
-        disabled={provider == null}
-        onClick={async () => {
-          if (!provider || !!signer) return;
-          const s = await provider.getSigner();
-          setSigner(s);
-        }}
-      >
-        {signer
-          ? `${signer.address.substring(0, 6)}...${signer.address.substr(-4)}`
-          : 'Connect wallet'}
-      </Button>
-
+    <Flex style={{ margin: '1rem' }} gap="3" align="center" justify="center">
       <Button
         disabled={state.status > AccountStatusEnum.NotSignedIn}
         onClick={() => {
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
-import { Container } from '@radix-ui/themes';
+import { Button, Container } from '@radix-ui/themes';
+import { BrowserProvider, JsonRpcSigner } from 'ethers';
+import { useEffect, useState } from 'react';
 
 import { Account } from './Account';
+import { checkValidNetwork } from './network';
 
 function App() {
+  const [provider, setProvider] = useState<BrowserProvider | undefined>();
+  const [signer, setSigner] = useState<JsonRpcSigner | undefined>();
+
+  useEffect(() => {
+    async function run() {
+      if (!window.ethereum) return;
+      const p = new BrowserProvider(window.ethereum);
+      setProvider(p);
+      const s = await p.getSigner();
+      setSigner(s);
+    }
+    run();
+  }, []);
+
+  useEffect(() => {
+    if (!provider) return;
+    checkValidNetwork(provider);
+  }, [provider]);
+
   return (
     <Container style={{ margin: '2rem' }}>
-      <Account />
+      <Button
+        disabled={provider == null}
+        onClick={async () => {
+          if (!provider || !!signer) return;
+          const s = await provider.getSigner();
+          setSigner(s);
+        }}
+      >
+        {signer
+          ? `${signer.address.substring(0, 6)}...${signer.address.substr(-4)}`
+          : 'Connect wallet'}
+      </Button>
+
+      <Account signer={signer} />
     </Container>
   );
 }
